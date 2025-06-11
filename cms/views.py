2436ebcbdc0bc26a36954bbd9b1b30a3b2d3dd3d
= <=>
2025-06-10 22:06:48 -0300

    ok

--- cms/views.py
+++ cms/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('<h1> BarberSites</h1>')
+    return HttpResponse('<h3> BarberSites</h3> <p> O App do Barbeiro Empreendedor </p>')
 
