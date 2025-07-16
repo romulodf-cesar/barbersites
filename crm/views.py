c20b7d3bb97f114ae6627e9f24b86a2579ca38b6
= <=>
2025-07-16 20:04:57 -0300

    plano concluido

--- crm/views.py
+++ crm/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render
 from .models import Plano
 
 def index(request):
-    return render(request, 'crm/index.html')
+    planos = Plano.objects.all().order_by('valor')
+
+    context = {
+        'planos': planos, # Passe a lista de planos para o template
+        # ... outros dados que você possa precisar no template (ex: formulários)
+    }
+    return render(request, 'crm/index.html',context)
 
 def checkout(request):
     return render(request, 'crm/checkout.html')
